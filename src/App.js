279ec30a3cfdc6b3e28ef668ccbe65fd7d29423e
AttAditya <attachment.aditya@gmail.com>
2024-06-21 11:19:29 +0530

    Simple counter and input app to learn useCallback and memo in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import { useState, useCallback } from 'react';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [input, setInput] = useState("");
+    let [count, setCount] = useState(0);
+
+    let incrementCount = useCallback(() => setCount(count + 1), [count]);
+
+    return (
+        <div className="App">
+            <input
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            />
+
+            <button onClick={incrementCount}>Increment</button>
+
+            <h3>Input: {input}</h3>
+            <h3>Count: {count}</h3>
+
+            <hr />
+
+            <ChildComponent
+                count={count}
+                incrementCount={incrementCount}
+            />
+        </div>
+    );
 }
+
+let ChildComponent = React.memo(function ({ count, incrementCount }) {
+    console.log("Child component rendered");
+
+    return (
+        <div>
+            <h2>This is a child component</h2>
+
+            <button onClick={incrementCount}>
+                Increment
+            </button>
+
+            <h4>Count: {count}</h4>
+        </div>
+    );
+})
 
 export default App;
